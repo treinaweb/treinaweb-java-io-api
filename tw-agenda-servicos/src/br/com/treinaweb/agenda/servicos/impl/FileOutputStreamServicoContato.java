0a08b951a2be2c7c4c2cbaa757ac386255256440
Cleber Campomori <cleber.campomori@gmail.com>
2019-01-17 14:50:20 -0200

    Aula 3.10: A classe FileOutputStream

---
+++ tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/impl/FileOutputStreamServicoContato.java
@@ -0,0 +1,24 @@
+package br.com.treinaweb.agenda.servicos.impl;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.treinaweb.agenda.entidades.Contato;
+import br.com.treinaweb.agenda.servicos.interfaces.ServicoContato;
+
+public class FileOutputStreamServicoContato implements ServicoContato {
+
+	@Override
+	public void exportar(List<Contato> contatos, String nomeArquivo) throws IOException {
+		try (FileOutputStream fileStream = new FileOutputStream(nomeArquivo)) {
+			List<String> dados = contatos.stream().map(contato -> String.format("%d;%s;%d;%s\n", contato.getId(),
+					contato.getNome(), contato.getIdade(), contato.getTelefone())).collect(Collectors.toList());
+			for (String dado : dados) {
+				fileStream.write(dado.getBytes());
+			}
+		}
+	}
+
+}
