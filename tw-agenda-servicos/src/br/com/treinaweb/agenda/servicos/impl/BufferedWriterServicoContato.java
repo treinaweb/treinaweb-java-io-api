42a4824946795098fb567bb446a59c2252a17246
Cleber Campomori <cleber.campomori@gmail.com>
2019-01-17 14:34:21 -0200

    Aula 3.6: As diferenças entre BufferedWriter e FileWriter

--- tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/impl/BufferedWriterServicoContato.java
+++ tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/impl/BufferedWriterServicoContato.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.agenda.servicos.impl;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,13 +15,14 @@ public class BufferedWriterServicoContato implements ServicoContato {
 	@Override
 	public void exportar(List<Contato> contatos, String nomeArquivo) throws IOException {
 		// BufferedWriter (Writer) --> FileWriter (apontador --> arquivo no HD
-		FileWriter fileWriter = new FileWriter(nomeArquivo);
-		try (BufferedWriter writer = new BufferedWriter(fileWriter)) {
-			List<String> dadosParaExportar = contatos.stream().map(contato -> String.format("%d;%s;%d;%s\n",
-					contato.getId(), contato.getNome(), contato.getIdade(), contato.getTelefone()))
-					.collect(Collectors.toList());
-			for (String linha : dadosParaExportar) {
-				writer.write(linha);
+		try (FileWriter fileWriter = new FileWriter(nomeArquivo)) {
+			try (BufferedWriter writer = new BufferedWriter(fileWriter)) {
+				List<String> dadosParaExportar = contatos.stream().map(contato -> String.format("%d;%s;%d;%s\n",
+						contato.getId(), contato.getNome(), contato.getIdade(), contato.getTelefone()))
+						.collect(Collectors.toList());
+				for (String linha : dadosParaExportar) {
+					writer.write(linha);
+				}
 			}
 		}
 	}
