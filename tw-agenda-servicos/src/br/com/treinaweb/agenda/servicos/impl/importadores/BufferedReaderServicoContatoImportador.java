3f160c57bcbabc2ef2f6b1c8ef8898a8ae2e7fae
Cleber Campomori <cleber.campomori@gmail.com>
2019-01-17 15:23:55 -0200

    Aula 4.2: Lendo arquivos com BufferedReader - parte 2

--- tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/impl/importadores/BufferedReaderServicoContatoImportador.java
+++ tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/impl/importadores/BufferedReaderServicoContatoImportador.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.agenda.servicos.impl.importadores;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import br.com.treinaweb.agenda.entidades.Contato;
 import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
@@ -11,9 +12,21 @@ import br.com.treinaweb.agenda.servicos.interfaces.ServicoImportadorContato;
 public class BufferedReaderServicoContatoImportador implements ServicoImportadorContato {
 
 	@Override
-	public void importar(String nomeArquivo, AgendaRepositorio<Contato> agendaRepositorio) throws IOException {
-		FileReader fileReader = new FileReader(nomeArquivo);
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
+	public void importar(String nomeArquivo, AgendaRepositorio<Contato> agendaRepositorio)
+			throws IOException, SQLException {
+		try (FileReader fileReader = new FileReader(nomeArquivo)) {
+			try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+				String dado = "";
+				while ((dado = bufferedReader.readLine()) != null) {
+					String[] informacoes = dado.split(";");
+					Contato contato = new Contato();
+					contato.setNome(informacoes[1]);
+					contato.setIdade(Integer.parseInt(informacoes[2]));
+					contato.setTelefone(informacoes[3]);
+					agendaRepositorio.inserir(contato);
+				}
+			}
+		}
 	}
 
 }
--- tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/interfaces/ServicoImportadorContato.java
+++ tw-agenda-servicos/src/br/com/treinaweb/agenda/servicos/interfaces/ServicoImportadorContato.java
@@ -1,12 +1,13 @@
 package br.com.treinaweb.agenda.servicos.interfaces;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import br.com.treinaweb.agenda.entidades.Contato;
 import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
 
 public interface ServicoImportadorContato {
 
-	void importar(String nomeArquivo, AgendaRepositorio<Contato> agendaRepositorio) throws IOException;
+	void importar(String nomeArquivo, AgendaRepositorio<Contato> agendaRepositorio) throws IOException, SQLException;
 
 }
